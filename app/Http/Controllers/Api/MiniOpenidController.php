b0dc4b80c2d622bc9707cecc24e00db2a6b05be1
HYH <h2005yh@126.com>
2021-10-20 05:05:27 +0000

    HYH 后端获取openid给小程序支付用6

--- app/Http/Controllers/Api/MiniOpenidController.php
+++ app/Http/Controllers/Api/MiniOpenidController.php
@@ -9,19 +9,21 @@ class MiniOpenidController extends Controller
 {
 //hyh小程序支付获取openid
     public function getOpenId(Request $request){
-        $code = $request->code;
+        $js_code = $request->js_code;
         $appid = env('MINIAPP_ID');
         $appsecret = env('MINIAPP_SECRET');
         $lll=env('WECHAT_PAY_KEY');
 
         file_put_contents("../hyh-appid.txt", var_export($lll,true));
         file_put_contents("../hyh-appsecret.txt", var_export($appsecret,true));
-//        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=".
-//            $appid."&secret=".$appsecret."&js_code=".$js_code."&grant_type=authorization_code";
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=".
+            $appid."&secret=".$appsecret."&js_code=".$js_code."&grant_type=authorization_code";
 
-        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".
-            $appid."&secret=".$appsecret."&code=".$code."&grant_type=authorization_code";
+//        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".
+//            $appid."&secret=".$appsecret."&code=".$code."&grant_type=authorization_code";
 
+
+//       https://api.weixin.qq.com/sns/jscode2session?appid=wx3f8c2c0a44cf1a50&secret=a82040dcb8c464b2b2e5b6444f159f5a&js_code=011EynFa1PuLXB0D6mGa1AEDD84EynF8&grant_type=authorization_code
 
         file_put_contents("../hyh-url.txt", var_export($url,true));
         //通过code换取网页授权access_token
