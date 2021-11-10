4240dce6716f7f172a67c0d6ffc90b4c2fe7ef64
HYH <h2005yh@126.com>
2021-11-10 06:22:15 +0000

    ceshi

--- app/Http/Middleware/FormatXiaochengxu.php
+++ app/Http/Middleware/FormatXiaochengxu.php
@@ -21,10 +21,10 @@ class FormatXiaochengxu
 
         $requestall = $request->all();
 
-        file_put_contents("../1234567890-chushi.txt", var_export($requestall,true));
+//        file_put_contents("../1234567890-chushi.txt", var_export($requestall,true));
         foreach ($requestall as $key => $value) {
 
-            $value=json_decode($value,true);
+//            $value=json_decode($value,true);
 
             $needle = "filter_";//判断是否包含filter_这个字符
             if (strpos($key, $needle) !== false) { //如果$key中存在filter_这个字符串
@@ -73,9 +73,9 @@ class FormatXiaochengxu
 
 
 //
-     file_put_contents("../1234567890-filter_new.txt", var_export($filter_new,true));
-     file_put_contents("../1234567891-requestall.txt", var_export($requestall,true));
-     file_put_contents("../1234567892-request.txt", var_export($request,true));
+//     file_put_contents("../1234567890-filter_new.txt", var_export($filter_new,true));
+//     file_put_contents("../1234567891-requestall.txt", var_export($requestall,true));
+//     file_put_contents("../1234567892-request.txt", var_export($request,true));
 
 
 //hyh小程序筛选条件改造 只针对当前接口可以是以下改法：
