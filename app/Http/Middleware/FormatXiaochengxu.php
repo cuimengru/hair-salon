f629122939b342fddf6d546d48786748a729846b
HYH <h2005yh@126.com>
2021-11-11 02:54:48 +0000

    作品筛选改造910

--- app/Http/Middleware/FormatXiaochengxu.php
+++ app/Http/Middleware/FormatXiaochengxu.php
@@ -23,16 +23,17 @@ class FormatXiaochengxu
 
 
 
-        $requestall['filter_gender'] = str_replace(array('[',']'),array('',''), $requestall['filter_gender']);
-        $requestall['filter_height_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_height_id']);
-        $requestall['filter_age_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_age_id']);
-        $requestall['filter_color_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_color_id']);
-        $requestall['filter_length_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_length_id']);
-        $requestall['filter_face_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_face_id']);
-        $requestall['filter_style_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_style_id']);
-        $requestall['filter_project_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_project_id']);
-        $requestall['filter_hair_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_hair_id']);
-        $requestall['filter_type'] = str_replace(array('[',']'),array('',''), $requestall['filter_type']);
+//        $requestall['filter_gender'] = str_replace(array('[',']'),array('',''), $requestall['filter_gender']);
+//        file_put_contents("../1234567890-filter.txt", var_export($requestall['filter_gender'],true));
+//        $requestall['filter_height_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_height_id']);
+//        $requestall['filter_age_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_age_id']);
+//        $requestall['filter_color_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_color_id']);
+//        $requestall['filter_length_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_length_id']);
+//        $requestall['filter_face_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_face_id']);
+//        $requestall['filter_style_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_style_id']);
+//        $requestall['filter_project_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_project_id']);
+//        $requestall['filter_hair_id'] = str_replace(array('[',']'),array('',''), $requestall['filter_hair_id']);
+//        $requestall['filter_type'] = str_replace(array('[',']'),array('',''), $requestall['filter_type']);
 
 
 
@@ -42,16 +43,17 @@ class FormatXiaochengxu
 
 //
 
-//            if(!empty($value)){
-//                $value = str_replace(array('[',']'),array('',''), $value);
-//            }
-//            else{
-//                $value=NULL;
-//            }
+            if(!empty($value)){
+                $value = str_replace(array('[',']'),array('',''), $value);
+            }
+            else{
+                $value=NULL;
+            }
 
 
+            $requestall[$key]=$value;
 
-
+            file_put_contents("../1234567890-requestall-new.txt", var_export($requestall,true));
 
             $needle = "filter_";//判断是否包含filter_这个字符
             if (strpos($key, $needle) !== false) { //如果$key中存在filter_这个字符串
